d243758adfe690fe10674fabeceede9edc07ef59
moeka2102 <moeka2102@gmail.com>
2018-07-31 02:47:16 -0400

    0731

---
+++ ex06-7.js
@@ -0,0 +1,17 @@
+var who ='world';
+function sayhello67(){
+alert('Hello,'+who+'!');
+}
+function someone(x){
+ who = x;
+
+}
+function レイ(){
+ who = 'レイ';
+}
+function シンジ(){
+ who = 'シンジ';
+}
+function アスカ(){
+ who = 'アスカ';
+}
---
+++ myjs.js
@@ -0,0 +1,4 @@
+alert('Hello,　Mai!');
+// alert('Hello, Yuza!');
+alert(2+3);
+alert(Math.sqrt(1+1));
